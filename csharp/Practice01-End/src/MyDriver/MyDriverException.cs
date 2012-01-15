8e1da1b0ae0bad84e0cd2f526378ca603e4b45a5
Jeffrey Zhao <jeffz@live.com>
2012-01-15 16:04:23 +0800

    Separate Begin and End

---
+++ csharp/Practice01-End/src/MyDriver/MyDriverException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyDriver
+{
+    public sealed class MyDriverException : Exception
+    {
+        public MyDriverException(string message)
+            : base(message)
+        { }
+    }
+}
---
+++ csharp/Practice01-End/src/MyDriver/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MyDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new MyDriver("jeffz://server:12345");
+
+            try
+            {
+                driver.Connect();
+                driver.AddQuery(1);
+                driver.AddQuery(2);
+                driver.AddQuery(3);
+            }
+            catch
+            {
+                driver.Close();
+                Console.WriteLine("Error occurred when connect or add query.");
+                Environment.Exit(1);
+            }
+
+            new Thread(() => ReceiveData(driver)).Start();
+        }
+
+        private static void ReceiveData(MyDriver driver)
+        {
+            try
+            {
+                while (true)
+                {
+                    var data = driver.Receive();
+                    if (data == null)
+                    {
+                        Console.WriteLine("Closed");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(data);
+                    }
+                }
+            }
+            catch (MyDriverException)
+            {
+                driver.Close();
+                Console.WriteLine("Error occurred when receive data.");
+            }
+        }
+    }
+}
