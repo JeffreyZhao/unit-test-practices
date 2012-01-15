fa3b99a153a098dd8d386bf2b420d6f9d4034aa7
Jeffrey Zhao <jeffz@live.com>
2012-01-15 20:47:57 +0800

    Init test commit

---
+++ csharp/Practice01-End/tests/MyClient.Tests/MyConnectionTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClient.Tests
+{
+    [TestClass]
+    public class MyConnectionTest
+    {
+        [TestMethod]
+        public void AlwaysSuccess()
+        {
+            
+        }
+    }
+}
